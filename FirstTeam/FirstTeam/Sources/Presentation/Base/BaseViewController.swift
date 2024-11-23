4175cd8945993a7680afd79dc40d2f1ebec8ffa3
mmaybei <yebbi2@ewhain.net>
2024-11-24 02:03:43 +0900

    feat: 네비게이션바 커스텀

--- FirstTeam/FirstTeam/Sources/Presentation/Base/BaseViewController.swift
+++ FirstTeam/FirstTeam/Sources/Presentation/Base/BaseViewController.swift
@@ -30,3 +30,44 @@ class BaseViewController: UIViewController {
     /// 오토레이아웃 설정 (SnapKit 코드 관련)
     func setLayout() {}
 }
+
+extension BaseViewController {
+    
+    /// 네비게이션 바 타이틀 및 배경색 설정
+    func setupNavigationBarTitle(with string: String) {
+        title = string
+        
+        let barAppearance = UINavigationBarAppearance()
+        barAppearance.shadowColor = nil
+        barAppearance.backgroundColor = UIColor(resource: .white0)
+        
+        barAppearance.titleTextAttributes = [
+            .foregroundColor: UIColor(resource: .black0),
+            .font: UIFont.pretendard(.title03)
+        ]
+        
+        navigationController?.navigationBar.standardAppearance = barAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = barAppearance
+        
+        setupNavigationBarBackButton()
+    }
+    
+    /// 네비게이션 바 BackButton 구성
+    func setupNavigationBarBackButton() {
+        let backButton = UIBarButtonItem(
+            image: UIImage(named: "ic_arrow_left"),
+            style: .plain,
+            target: self,
+            action: #selector(backButtonDidTap)
+        ).then {
+            $0.tintColor = UIColor(resource: .black0)
+        }
+        
+        navigationItem.leftBarButtonItem = backButton
+    }
+    
+    @objc
+    func backButtonDidTap() {
+        navigationController?.popViewController(animated: true)
+    }
+}
