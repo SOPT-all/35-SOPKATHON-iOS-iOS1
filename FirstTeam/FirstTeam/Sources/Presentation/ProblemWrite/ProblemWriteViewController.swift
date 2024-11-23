e404ab44a98a5a5d83204db14a7dfe0f4c4ddc7b
mmaybei <yebbi2@ewhain.net>
2024-11-24 01:15:07 +0900

    feat: 키보드 기능 구현

--- FirstTeam/FirstTeam/Sources/Presentation/ProblemWrite/ProblemWriteViewController.swift
+++ FirstTeam/FirstTeam/Sources/Presentation/ProblemWrite/ProblemWriteViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 final class ProblemWriteViewController: BaseViewController {
     
@@ -21,6 +22,12 @@ final class ProblemWriteViewController: BaseViewController {
         super.viewDidLoad()
         
         setTarget()
+        setGesture()
+        addKeyboardObservers()
+    }
+    
+    deinit {
+        removeKeyboardObservers()
     }
     
     private func setTarget() {
@@ -48,6 +55,26 @@ final class ProblemWriteViewController: BaseViewController {
         }
     }
     
+    private func addKeyboardObservers() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow(_:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide(_:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+    
+    private func removeKeyboardObservers() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
     @objc
     private func textFieldDidChange(_ textField: UITextField) {
         checkValid(text: rootView.titleTextField.text)
@@ -56,5 +83,31 @@ final class ProblemWriteViewController: BaseViewController {
     @objc
     private func dismissKeyboard() {
         view.endEditing(true)
+    }
+    
+    @objc
+    private func keyboardWillShow(_ notification: Notification) {
+        guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect,
+              let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else { return }
+        
+        let keyboardHeight = keyboardFrame.height
+        UIView.animate(withDuration: animationDuration) {
+            self.rootView.nextButton.snp.updateConstraints {
+                $0.bottom.equalToSuperview().inset(keyboardHeight + 20) // 키보드 위 20pt 여백
+            }
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc
+    private func keyboardWillHide(_ notification: Notification) {
+        guard let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else { return }
+        
+        UIView.animate(withDuration: animationDuration) {
+            self.rootView.nextButton.snp.updateConstraints {
+                $0.bottom.equalToSuperview().inset(44) // 원래 위치로 복원
+            }
+            self.view.layoutIfNeeded()
+        }
     }
 }
