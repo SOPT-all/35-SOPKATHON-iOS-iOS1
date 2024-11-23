f77697e63573472b788255c3dea169fa83839d64
mmaybei <yebbi2@ewhain.net>
2024-11-24 05:11:40 +0900

    refactor: 엠티뷰 병합

--- FirstTeam/FirstTeam/Sources/Presentation/ProblemList/ProblemListView.swift
+++ FirstTeam/FirstTeam/Sources/Presentation/ProblemList/ProblemListView.swift
@@ -17,18 +17,31 @@ final class ProblemListView: BaseView {
         $0.separatorStyle = .none
     }
     
+    private let emptyLabel = UILabel().then {
+        $0.text = "아직 해결된 고민이 없삼...\n고민을 입력해 주삼!"
+        $0.font = .pretendard(.title01)
+        $0.textAlignment = .center
+        $0.numberOfLines = 2
+        $0.isHidden = true
+    }
+    
     override func setStyle() {
         backgroundColor = UIColor(resource: .white0)
     }
     
     override func setUI() {
-        addSubview(tableView)
+        addSubviews(tableView, emptyLabel)
     }
     
     override func setLayout() {
         tableView.snp.makeConstraints {
             $0.top.equalToSuperview().inset(100)
             $0.leading.trailing.bottom.equalToSuperview().inset(20)
+        }
+        
+        emptyLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(336)
+            $0.centerX.equalToSuperview()
         }
     }
 }
