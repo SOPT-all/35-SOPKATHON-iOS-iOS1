4af047a55b6cae9f029710c12878d828e70122da
hongseekim <songhee0617@gmail.com>
2024-11-24 04:46:21 +0900

    feat: 스플래쉬 화면 구현

--- FirstTeam/FirstTeam/Sources/Presentation/Splash/ViewController/SplashViewController.swift
+++ FirstTeam/FirstTeam/Sources/Presentation/Splash/ViewController/SplashViewController.swift
@@ -7,18 +7,48 @@
 
 import UIKit
 
-final class SplashViewController: UIViewController {
+final class SplashViewController: BaseViewController {
+    
+    private let logoImageView = UIImageView().then {
+        $0.image = UIImage(resource: .splashLogo)
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    private let characterImageView = UIImageView().then {
+        $0.image = UIImage(resource: .character)
+        $0.contentMode = .scaleAspectFit
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .green
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
             self.navigateToNicknameViewController()
         }
     }
     
+    override func setStyle() {
+        view.backgroundColor = UIColor(resource: .green0)
+    }
+
+    override func setUI() {
+        view.addSubviews(logoImageView, characterImageView)
+    }
+
+    override func setLayout() {
+        logoImageView.snp.makeConstraints{
+            $0.top.equalToSuperview().inset(224)
+            $0.leading.equalToSuperview().inset(92)
+            $0.height.equalTo(46)
+            $0.width.equalTo(192)
+        }
+        
+        characterImageView.snp.makeConstraints{
+            $0.top.equalToSuperview().inset(426)
+            $0.centerX.equalToSuperview()
+        }
+    }
+
     private func navigateToNicknameViewController() {
         
         var nextViewController = UIViewController()
@@ -27,7 +57,7 @@ final class SplashViewController: UIViewController {
             if token.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
                 nextViewController = NicknameViewController()
             } else {
-                // TODO: set home view controller as nextViewController
+                nextViewController = HomeViewController()
             }
 
         let navigationController = UINavigationController(rootViewController: nextViewController)
