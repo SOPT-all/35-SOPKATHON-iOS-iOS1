e72648d4419642cfe55c59113d3e215c71816688
mmaybei <yebbi2@ewhain.net>
2024-11-24 03:36:14 +0900

    feat: 고민 리스트 구현

---
+++ FirstTeam/FirstTeam/Sources/Presentation/ProblemList/ProblemListView.swift
@@ -0,0 +1,34 @@
+//
+//  ProblemListView.swift
+//  FirstTeam
+//
+//  Created by 예삐 on 11/24/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class ProblemListView: BaseView {
+    let tableView = UITableView(frame: .zero, style: .plain).then {
+        $0.backgroundColor = .clear
+        $0.showsVerticalScrollIndicator = false
+        $0.separatorStyle = .none
+    }
+    
+    override func setStyle() {
+        backgroundColor = UIColor(resource: .white0)
+    }
+    
+    override func setUI() {
+        addSubview(tableView)
+    }
+    
+    override func setLayout() {
+        tableView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(70)
+            $0.leading.trailing.bottom.equalToSuperview().inset(20)
+        }
+    }
+}
